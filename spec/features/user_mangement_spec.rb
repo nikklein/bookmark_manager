c0dfbf59e4b282b5a64d43893a28c073efebcca9
nikklein <objcoding@gmail.com>
2016-07-27 17:41:03 +0100

    Chapter 19 in progress

--- spec/features/user_mangement_spec.rb
+++ spec/features/user_mangement_spec.rb
@@ -4,4 +4,13 @@ feature 'User sign up' do
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
+  scenario "I can't signup without confirmation" do
+  visit '/users/new'
+  expect(page.status_code).to eq(200)
+  fill_in :email, with: 'alice@example.com'
+  fill_in :password, with: 'oranges!'
+  fill_in :password_confirmation, with: 'orange!'
+  expect { sign_up }.to change(User, :count).by(0)
+end
+
 end
