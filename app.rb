9bbb6424c7b4af80a9c7e4ecbeec650d8bca265b
Mali Michael <malimccalla@gmail.com>
2016-07-27 17:26:35 +0100

    add user sign in

--- app.rb
+++ app.rb
@@ -44,6 +44,12 @@ class BookmarkManager < Sinatra::Base
    erb :'users/new'
  end
 
+ helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
 
   run! if app_file == $0
 end
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -4,20 +4,17 @@ feature 'adding a tag' do
     fill_in 'url', with: 'www.facebook.com'
     fill_in 'title', with: 'Facebook'
     fill_in 'tags', with: 'social'
-
     click_button 'Create link'
     link = Link.first
     expect(link.tags.map(&:name)).to include 'social'
   end
-
-scenario 'I can add multiple tags to a new link' do
-  visit '/links/new'
-  fill_in 'url',   with: 'http://www.makersacademy.com/'
-  fill_in 'title', with: 'Makers Academy'
-  fill_in 'tags',  with: 'education ruby'
-  click_button 'Create link'
-  link = Link.first
-  expect(link.tags.map(&:name)).to include('education', 'ruby')
-end
-
+  scenario 'I can add multiple tags' do
+    visit 'links/new'
+    fill_in 'url', with: 'www.facebook.com'
+    fill_in 'title', with: 'Facebook'
+    fill_in 'tags', with: 'social internet user'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('social', 'user', 'internet')
+  end
 end
