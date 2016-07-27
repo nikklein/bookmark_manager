3b4796d97f56cb61b6590c63aded32b5c671fdb1
nikklein <objcoding@gmail.com>
2016-07-27 16:56:43 +0100

    Update

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -4,17 +4,20 @@ feature 'adding a tag' do
     fill_in 'url', with: 'www.facebook.com'
     fill_in 'title', with: 'Facebook'
     fill_in 'tags', with: 'social'
+
     click_button 'Create link'
     link = Link.first
     expect(link.tags.map(&:name)).to include 'social'
   end
-  scenario 'I can add multiple tags' do
-    visit 'links/new'
-    fill_in 'url', with: 'www.facebook.com'
-    fill_in 'title', with: 'Facebook'
-    fill_in 'tags', with: 'social internet user'
-    click_button 'Create link'
-    link = Link.first
-    expect(link.tags.map(&:name)).to include('social', 'user', 'internet')
-  end
+
+scenario 'I can add multiple tags to a new link' do
+  visit '/links/new'
+  fill_in 'url',   with: 'http://www.makersacademy.com/'
+  fill_in 'title', with: 'Makers Academy'
+  fill_in 'tags',  with: 'education ruby'
+  click_button 'Create link'
+  link = Link.first
+  expect(link.tags.map(&:name)).to include('education', 'ruby')
+end
+
 end
