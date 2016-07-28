51e48da84af2c76c70d3ac9dc2b60a0c2c12ed80
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-28 10:46:24 +0100

    completed task 18

--- app/app.rb
+++ app/app.rb
@@ -1,22 +1,30 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-# require_relative 'models/link'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
 
   get '/' do
-    redirect '/signup'
+    redirect '/users/new'
   end
 
-  get '/signup' do
-    erb :'links/signup'
+  get '/users/new' do
+    erb :'users/new'
   end
 
-  post '/navigation' do
-    user = User.create(username: params[:username], password: params[:password])
-    redirect '/navigation'
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
   end
 
   get '/navigation' do
--- spec/features/add_user_spec.rb
+++ spec/features/add_user_spec.rb
@@ -1,22 +1,18 @@
 require './spec/spec_helper'
+require './spec/web_helper'
 
 feature 'User sign up' do
   scenario 'User can visit sign up page' do
-    visit '/signup'
+    visit '/users/new'
     expect(page.status_code).to eq 200
-    find_field('Username').value
+    find_field('Email').value
     find_field('Password').value
-    expect(page).to have_button('Sign up!')
+    expect(page).to have_button('Sign up')
   end
 
   scenario 'New user is created' do
-    visit '/signup'
-    fill_in 'Username', with: 'user@email.com'
-    fill_in 'Password', with: 'Password123'
-    click_button 'Sign up!'
-    user = User.all
-    expect(current_path).to eq '/navigation'
-    expect(user.map(&:username)).to include('user@email.com')
-    expect(user.map(&:password)).not_to be('Password123')
+    expect{ sign_up }.to change(User, :count).by(1)
+    expect(page).to have_content('Welcome, user@email.com')
+    expect(User.first.email).to eq('user@email.com')
   end
 end
